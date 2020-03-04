1b27c5df92dc10f334cd323933f8fc764761263c
Serj <vvirus@ukr.net>
2020-03-04 14:22:24 +0200

    edit cnf

--- lesson11/DbConnect.php
+++ lesson11/DbConnect.php
@@ -10,7 +10,7 @@ class DbConnect
     {
         $dsn = 'mysql:host=app-mysql;port=3306;dbname=hillel';
 
-        $pdo = new \PDO($dsn, 'hillel', 'PitstOp1234');
+        $pdo = new \PDO($dsn, 'hillel', '****');
         $pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
         $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         return $pdo;
